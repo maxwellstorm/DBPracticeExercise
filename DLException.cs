2e53160b5fb38206428330be40a81138f07932f5
maxwellstorm <mxs5194@rit.edu>
2015-03-10 09:42:11 -0400

    started exception handling

---
+++ DLException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SQLConnect
+{
+    class DLException
+    {
+        public DLException(Exception e)
+        {
+
+        }
+
+        public DLException(Exception e, params string[] paramList)
+        {
+
+        }
+
+        public void log()
+        {
+            StreamWriter sw = File.
+
+            
+           
+            
+        }
+    }
+}
